a5b3420b070e86a73035b98ebf411830d5898062
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-05-11 18:01:34 -0300

    dist

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import * as azure_native from "@pulumi/azure-native";
 
 const config = new pulumi.Config();
 
-// Create an Azure Resource Group
 const resourceGroup = new azure_native.resources.ResourceGroup("rg-static-webapp", {
     location: "westus2"
 });
@@ -11,11 +10,10 @@ const resourceGroup = new azure_native.resources.ResourceGroup("rg-static-webapp
 const staticSite = new azure_native.web.StaticSite("staticsite-demo", {
     resourceGroupName: resourceGroup.name,
     location: "westus2",
-    // repositoryToken: config.requireSecret("GITHUB_ACCESS_TOKEN"),
     repositoryUrl: "https://github.com/epomatti/az-static-webapps-pulumi",
     branch: "main",
     buildProperties: {
-        // appArtifactLocation: "build",
+        appArtifactLocation: "dist",
         appLocation: "frontend",
     },
     sku: {
@@ -24,8 +22,3 @@ const staticSite = new azure_native.web.StaticSite("staticsite-demo", {
     },
 
 })
-
-// // Export the primary key of the Storage Account
-// const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-//     storage.listStorageAccountKeys({ resourceGroupName, accountName }));
-// export const primaryStorageKey = storageAccountKeys.keys[0].value;
