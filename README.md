994a452a62baa379a82e790824d3edf861f7e77e
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-05-12 10:23:10 -0300

    fixes

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Private Azure Static Web + Pulumi
+# Pulumi on Azure - Private Endpoints, Static Web
 
+Private static web app, accessible via Private Endpoints.
+
+### Deployment
 
 1 - Create the infrastructure with Pulumi:
 
@@ -12,9 +15,16 @@ pulumi up -y
 
 2 - Once the Static Web App is deployed, copy the deployment token `AZURE_STATIC_WEB_APPS_API_TOKEN` to GitHub as an Action secret. Triggering the pipeline will deploy the code to Azure.
 
-3 - Site should be available via Private Endpoint
+3 - Site should be available via Private Endpoint - Use the Jumpbox VM for testing.
+
+
+---
+
+### Source
 
 ```source
+https://docs.microsoft.com/en-us/azure/purview/catalog-private-link-name-resolution
+https://docs.microsoft.com/en-us/azure/private-link/troubleshoot-private-endpoint-connectivity
 https://docs.microsoft.com/en-us/azure/private-link/private-endpoint-dns
 https://docs.microsoft.com/en-us/azure/dns/private-dns-autoregistration
 https://github.com/pulumi/examples/tree/master/azure-ts-webapp-privateendpoint-vnet-injection
--- index.ts
+++ index.ts
@@ -2,24 +2,33 @@ import * as pulumi from "@pulumi/pulumi";
 import * as azure_native from "@pulumi/azure-native";
 
 const resourceGroup = new azure_native.resources.ResourceGroup("rg-static-webapp", {
-    location: "westus2"
 });
+
+// Networking Resources
 
 const virtualNetwork = new azure_native.network.VirtualNetwork("virtualNetwork", {
     addressSpace: {
         addressPrefixes: ["10.0.0.0/16"],
     },
-    location: "westus2",
     resourceGroupName: resourceGroup.name,
     virtualNetworkName: "vnet-static-webapp",
 });
 
-const subnet = new azure_native.network.Subnet("subnet-001", {
+const subnetStaticSite = new azure_native.network.Subnet("subnet-001-site", {
     addressPrefix: "10.0.0.0/24",
     privateEndpointNetworkPolicies: azure_native.network.VirtualNetworkPrivateEndpointNetworkPolicies.Disabled,
     resourceGroupName: resourceGroup.name,
     virtualNetworkName: virtualNetwork.name,
 });
+
+const subnetVm = new azure_native.network.Subnet("subnet-002-vm", {
+    addressPrefix: "10.0.10.0/24",
+    privateEndpointNetworkPolicies: azure_native.network.VirtualNetworkPrivateEndpointNetworkPolicies.Enabled,
+    resourceGroupName: resourceGroup.name,
+    virtualNetworkName: virtualNetwork.name,
+});
+
+// Private DNS
 
 const privateZone = new azure_native.network.PrivateZone("privateZone", {
     location: "Global",
@@ -27,9 +36,21 @@ const privateZone = new azure_native.network.PrivateZone("privateZone", {
     resourceGroupName: resourceGroup.name,
 });
 
+const virtualNetworkLink = new azure_native.network.VirtualNetworkLink("virtualNetworkLink", {
+    location: "Global",
+    privateZoneName: privateZone.name,
+    registrationEnabled: true,
+    resourceGroupName: resourceGroup.name,
+    virtualNetwork: {
+        id: virtualNetwork.id,
+    },
+    virtualNetworkLinkName: "virtualNetworkLink1",
+});
+
+// Private Static WebSite
+
 const staticSite = new azure_native.web.StaticSite("staticsite-demo", {
     resourceGroupName: resourceGroup.name,
-    location: "westus2",
     repositoryUrl: "https://github.com/epomatti/az-static-webapps-pulumi",
     branch: "main",
     buildProperties: {
@@ -42,38 +63,75 @@ const staticSite = new azure_native.web.StaticSite("staticsite-demo", {
     },
 })
 
-const virtualNetworkLink = new azure_native.network.VirtualNetworkLink("virtualNetworkLink", {
-    location: "Global",
-    privateZoneName: privateZone.name,
-    registrationEnabled: false,
-    resourceGroupName: resourceGroup.name,
-    virtualNetwork: {
-        id: virtualNetwork.id,
-    },
-    virtualNetworkLinkName: "virtualNetworkLink1",
-});
+// const privateEndpoint = new azure_native.network.PrivateEndpoint("privateEndpoint", {
+//     privateLinkServiceConnections: [{
+//         groupIds: ["staticSites"],
+//         name: "privateEndpointLink1",
+//         privateLinkServiceId: staticSite.id,
+//     }],
+//     // customDnsConfigs: [
 
-const privateEndpoint = new azure_native.network.PrivateEndpoint("privateEndpoint", {
-    location: "westus2",
-    privateLinkServiceConnections: [{
-        groupIds: ["staticSites"],
-        name: "privateEndpointLink1",
-        privateLinkServiceId: staticSite.id,
-    }],
-    privateEndpointName: "pe-staticsite",
-    resourceGroupName: resourceGroup.name,
-    subnet: {
-        id: subnet.id,
-    },
-});
+//     // ],
+//     privateEndpointName: "pe-staticsite",
+//     resourceGroupName: resourceGroup.name,
+//     subnet: {
+//         id: subnet.id,
+//     }
+// });
 
+// const privateDNSZoneGroup = new azure_native.network.PrivateDnsZoneGroup("privateDnsZoneGroup", {
+//     privateDnsZoneConfigs: [{
+//         name: "config1",
+//         privateDnsZoneId: privateZone.id,
+//     }],
+//     privateDnsZoneGroupName: privateEndpoint.name,
+//     privateEndpointName: privateEndpoint.name,
+//     resourceGroupName: resourceGroup.name,
+// });
 
-const privateDNSZoneGroup = new azure_native.network.PrivateDnsZoneGroup("privateDnsZoneGroup", {
-    privateDnsZoneConfigs: [{
-        name: "config1",
-        privateDnsZoneId: privateZone.id,
-    }],
-    privateDnsZoneGroupName: privateEndpoint.name,
-    privateEndpointName: privateEndpoint.name,
-    resourceGroupName: resourceGroup.name,
-});
+// Jumpbox VM for Testing
+
+// const publicIp = new azure_native.network.PublicIPAddress("server-ip", {
+//     resourceGroupName: resourceGroup.name,
+//     publicIPAllocationMethod: azure_native.network.IPAllocationMethod.Dynamic,
+// });
+
+// const networkInterface = new azure_native.network.NetworkInterface("server-nic", {
+//     resourceGroupName: resourceGroup.name,
+//     ipConfigurations: [{
+//         name: "webserveripcfg",
+//         subnet: { id: subnetVM.id },
+//         privateIPAllocationMethod: azure_native.network.IPAllocationMethod.Dynamic,
+//         publicIPAddress: { id: publicIp.id },
+//     }],
+// });
+
+// const vm = new azure_native.compute.VirtualMachine("server-vm", {
+//     resourceGroupName: resourceGroup.name,
+//     networkProfile: {
+//         networkInterfaces: [{ id: networkInterface.id }],
+//     },
+//     hardwareProfile: {
+//         vmSize: azure_native.compute.VirtualMachineSizeTypes.Standard_B2s,
+//     },
+//     osProfile: {
+//         computerName: "hostname",
+//         adminUsername: "azadm",
+//         adminPassword: "StrongPass#789",
+//         linuxConfiguration: {
+//             disablePasswordAuthentication: false,
+//         },
+//     },
+//     storageProfile: {
+//         osDisk: {
+//             createOption: azure_native.compute.DiskCreateOption.FromImage,
+//             name: "myosdisk1",
+//         },
+//         imageReference: {
+//             publisher: "Canonical",
+//             offer: "0001-com-ubuntu-server-focal",
+//             sku: "20_04-lts-gen2",
+//             version: "latest",
+//         },
+//     },
+// });
