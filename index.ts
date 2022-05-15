3ee90d966ca838c039ce3e6681ec3fd6827a183b
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-05-14 23:50:40 -0300

    gateway

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure_native from "@pulumi/azure-native";
 
+let config = new pulumi.Config();
+
 const resourceGroup = new azure_native.resources.ResourceGroup("rg-static-webapp", {
 });
 
@@ -28,7 +30,14 @@ const subnetVm = new azure_native.network.Subnet("subnet-002-vm", {
     virtualNetworkName: virtualNetwork.name,
 });
 
-// Private DNS
+const subnetGateway = new azure_native.network.Subnet("App-Gateway-Subnet", {
+    addressPrefix: "10.0.90.0/27",
+    privateEndpointNetworkPolicies: azure_native.network.VirtualNetworkPrivateEndpointNetworkPolicies.Enabled,
+    resourceGroupName: resourceGroup.name,
+    virtualNetworkName: virtualNetwork.name,
+});
+
+// Private DNS - Azure Web
 
 const privateZone = new azure_native.network.PrivateZone("privateZone", {
     location: "global",
@@ -38,7 +47,7 @@ const privateZone = new azure_native.network.PrivateZone("privateZone", {
     dependsOn: [virtualNetwork],
 });
 
-const virtualNetworkLink = new azure_native.network.VirtualNetworkLink("virtualNetworkLink", {
+const virtualNetworkLinkGateway = new azure_native.network.VirtualNetworkLink("virtualNetworkLink", {
     location: "global",
     privateZoneName: privateZone.name,
     registrationEnabled: true,
@@ -47,6 +56,26 @@ const virtualNetworkLink = new azure_native.network.VirtualNetworkLink("virtualN
         id: virtualNetwork.id,
     },
     virtualNetworkLinkName: "virtualNetworkLink1",
+});
+
+// Private DNS - Gateway
+const privateZoneGateway = new azure_native.network.PrivateZone("privateZoneGateway", {
+    location: "global",
+    privateZoneName: "intranet.mycompany.com",
+    resourceGroupName: resourceGroup.name
+}, {
+    dependsOn: [virtualNetwork],
+});
+
+const virtualNetworkLink = new azure_native.network.VirtualNetworkLink("virtualNetworkLinkGateway", {
+    location: "global",
+    privateZoneName: privateZoneGateway.name,
+    registrationEnabled: true,
+    resourceGroupName: resourceGroup.name,
+    virtualNetwork: {
+        id: virtualNetwork.id,
+    },
+    virtualNetworkLinkName: "virtualNetworkLinkGateway",
 });
 
 // Private Static WebSite
@@ -87,50 +116,264 @@ const privateDNSZoneGroup = new azure_native.network.PrivateDnsZoneGroup("privat
     resourceGroupName: resourceGroup.name,
 });
 
+// Application Gateway
 
-// Jumpbox VM for Testing
+const applicationGatewayName = config.require("appGWName");
 
-const publicIp = new azure_native.network.PublicIPAddress("server-ip", {
+const buildResourceId = (type: string, name: string) => {
+    return pulumi.interpolate`${resourceGroup.id}/providers/Microsoft.Network/applicationGateways/${applicationGatewayName}/${type}/${name}`
+}
+
+const applicationGateway = new azure_native.network.ApplicationGateway("applicationGateway", {
+    applicationGatewayName: applicationGatewayName,
+    backendAddressPools: [{
+        backendAddresses: [
+            {
+                fqdn: staticSite.defaultHostname
+            },
+        ],
+        name: "appgwpool",
+    }],
+    backendHttpSettingsCollection: [{
+        cookieBasedAffinity: "Disabled",
+        name: "appgwbhs",
+        port: 443,
+        protocol: "Https",
+        requestTimeout: 30,
+    }],
+    frontendIPConfigurations: [{
+        name: "appgwfip",
+        // publicIPAddress: {
+        //     id: pulumi.interpolate`${resourceGroup.id}/providers/Microsoft.Network/publicIPAddresses/appgwpip`,
+        // },
+        privateIPAddress: "10.0.90.100"
+    }],
+    frontendPorts: [
+        // {
+        //     name: "appgwfp",
+        //     port: 443,
+        // },
+        {
+            name: "appgwfp80",
+            port: 80,
+        },
+    ],
+    gatewayIPConfigurations: [{
+        name: "appgwipc",
+        subnet: {
+            id: pulumi.interpolate`${subnetGateway.id}`,
+        },
+    }],
+    httpListeners: [
+        // {
+        //     frontendIPConfiguration: {
+        //         id: buildResourceId("frontendIPConfigurations", "appgwfip"),
+        //     },
+        //     frontendPort: {
+        //         id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/applicationGateways/appgw/frontendPorts/appgwfp",
+        //     },
+        //     name: "appgwhl",
+        //     protocol: "Https",
+        //     requireServerNameIndication: false,
+        //     sslCertificate: {
+        //         id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/applicationGateways/appgw/sslCertificates/sslcert",
+        //     },
+        //     sslProfile: {
+        //         id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/applicationGateways/appgw/sslProfiles/sslProfile1",
+        //     },
+        // },
+        {
+            frontendIPConfiguration: {
+                id: buildResourceId("frontendIPConfigurations", "appgwfip"),
+            },
+            frontendPort: {
+                id: buildResourceId("frontendPorts", "appgwfp80"),
+            },
+            name: "appgwhttplistener",
+            protocol: "Http",
+        },
+    ],
+    // identity: {
+    //     type: azure_native.network.ResourceIdentityType.UserAssigned,
+    //     userAssignedIdentities: {
+    //         "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.ManagedIdentity/userAssignedIdentities/identity1": {},
+    //     },
+    // },
+    requestRoutingRules: [
+        {
+            backendAddressPool: {
+                id: buildResourceId("backendAddressPools", "appgwpool"),
+            },
+            backendHttpSettings: {
+                id: buildResourceId("backendHttpSettingsCollection", "appgwbhs"),
+            },
+            httpListener: {
+                id: buildResourceId("httpListeners", "appgwhl"),
+            },
+            name: "appgwrule",
+            priority: 10,
+            rewriteRuleSet: {
+                id: buildResourceId("rewriteRuleSets", "rewriteRuleSet1"),
+            },
+            ruleType: "Basic",
+        },
+        {
+            httpListener: {
+                id: buildResourceId("httpListeners", "appgwhttplistener"),
+            },
+            name: "appgwPathBasedRule",
+            priority: 20,
+            ruleType: "PathBasedRouting",
+            urlPathMap: {
+                id: buildResourceId("urlPathMaps", "pathMap1"),
+            },
+        },
+    ],
     resourceGroupName: resourceGroup.name,
-    publicIPAllocationMethod: azure_native.network.IPAllocationMethod.Dynamic,
-});
-
-const networkInterface = new azure_native.network.NetworkInterface("server-nic", {
-    resourceGroupName: resourceGroup.name,
-    ipConfigurations: [{
-        name: "webserveripcfg",
-        subnet: { id: subnetVm.id },
-        privateIPAllocationMethod: azure_native.network.IPAllocationMethod.Dynamic,
-        publicIPAddress: { id: publicIp.id },
+    rewriteRuleSets: [{
+        name: "rewriteRuleSet1",
+        rewriteRules: [{
+            actionSet: {
+                requestHeaderConfigurations: [{
+                    headerName: "X-Forwarded-For",
+                    headerValue: "{var_add_x_forwarded_for_proxy}",
+                }],
+                responseHeaderConfigurations: [{
+                    headerName: "Strict-Transport-Security",
+                    headerValue: "max-age=31536000",
+                }],
+                urlConfiguration: {
+                    modifiedPath: "/abc",
+                },
+            },
+            conditions: [{
+                ignoreCase: true,
+                negate: false,
+                pattern: "^Bearer",
+                variable: "http_req_Authorization",
+            }],
+            name: "Set X-Forwarded-For",
+            ruleSequence: 102,
+        }],
+    }],
+    sku: {
+        capacity: 1,
+        name: "Basic",
+        tier: "Basic",
+    },
+    // sslCertificates: [
+    //     {
+    //         data: "****",
+    //         name: "sslcert",
+    //         password: "****",
+    //     },
+    //     {
+    //         keyVaultSecretId: "https://kv/secret",
+    //         name: "sslcert2",
+    //     },
+    // ],
+    // sslProfiles: [{
+    //     clientAuthConfiguration: {
+    //         verifyClientCertIssuerDN: true,
+    //     },
+    //     name: "sslProfile1",
+    //     sslPolicy: {
+    //         cipherSuites: ["TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"],
+    //         minProtocolVersion: "TLSv1_1",
+    //         policyType: "Custom",
+    //     },
+    //     trustedClientCertificates: [{
+    //         id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/applicationGateways/appgw/trustedClientCertificates/clientcert",
+    //     }],
+    // }],
+    // trustedClientCertificates: [{
+    //     data: "****",
+    //     name: "clientcert",
+    // }],
+    // trustedRootCertificates: [
+    //     {
+    //         data: "****",
+    //         name: "rootcert",
+    //     },
+    //     {
+    //         keyVaultSecretId: "https://kv/secret",
+    //         name: "rootcert1",
+    //     },
+    // ],
+    urlPathMaps: [{
+        defaultBackendAddressPool: {
+            id: buildResourceId("backendAddressPools", "appgwpool"),
+        },
+        defaultBackendHttpSettings: {
+            id: buildResourceId("backendHttpSettingsCollection", "appgwbhs"),
+        },
+        defaultRewriteRuleSet: {
+            id: buildResourceId("rewriteRuleSets", "rewriteRuleSet1"),
+        },
+        name: "pathMap1",
+        pathRules: [{
+            backendAddressPool: {
+                id: buildResourceId("backendAddressPools", "appgwpool"),
+            },
+            backendHttpSettings: {
+                id: buildResourceId("backendHttpSettingsCollection", "appgwbhs"),
+            },
+            name: "apiPaths",
+            paths: [
+                "/api",
+                "/v1/api",
+            ],
+            rewriteRuleSet: {
+                id: buildResourceId("rewriteRuleSets", "rewriteRuleSet1"),
+            },
+        }],
     }],
 });
 
-const vm = new azure_native.compute.VirtualMachine("server-vm", {
-    resourceGroupName: resourceGroup.name,
-    networkProfile: {
-        networkInterfaces: [{ id: networkInterface.id }],
-    },
-    hardwareProfile: {
-        vmSize: azure_native.compute.VirtualMachineSizeTypes.Standard_B2s,
-    },
-    osProfile: {
-        computerName: "hostname",
-        adminUsername: "azadm",
-        adminPassword: "StrongPass#789",
-        linuxConfiguration: {
-            disablePasswordAuthentication: false,
-        },
-    },
-    storageProfile: {
-        osDisk: {
-            createOption: azure_native.compute.DiskCreateOption.FromImage,
-            name: "myosdisk1",
-        },
-        imageReference: {
-            publisher: "Canonical",
-            offer: "0001-com-ubuntu-server-focal",
-            sku: "20_04-lts-gen2",
-            version: "latest",
-        },
-    },
-});
+
+// Jumpbox VM for Testing
+
+// const publicIp = new azure_native.network.PublicIPAddress("server-ip", {
+//     resourceGroupName: resourceGroup.name,
+//     publicIPAllocationMethod: azure_native.network.IPAllocationMethod.Dynamic,
+// });
+
+// const networkInterface = new azure_native.network.NetworkInterface("server-nic", {
+//     resourceGroupName: resourceGroup.name,
+//     ipConfigurations: [{
+//         name: "webserveripcfg",
+//         subnet: { id: subnetVm.id },
+//         privateIPAllocationMethod: azure_native.network.IPAllocationMethod.Dynamic,
+//         publicIPAddress: { id: publicIp.id },
+//     }],
+// });
+
+// const vm = new azure_native.compute.VirtualMachine("server-vm", {
+//     resourceGroupName: resourceGroup.name,
+//     networkProfile: {
+//         networkInterfaces: [{ id: networkInterface.id }],
+//     },
+//     hardwareProfile: {
+//         vmSize: azure_native.compute.VirtualMachineSizeTypes.Standard_B2s,
+//     },
+//     osProfile: {
+//         computerName: "hostname",
+//         adminUsername: "azadm",
+//         adminPassword: "StrongPass#789",
+//         linuxConfiguration: {
+//             disablePasswordAuthentication: false,
+//         },
+//     },
+//     storageProfile: {
+//         osDisk: {
+//             createOption: azure_native.compute.DiskCreateOption.FromImage,
+//             name: "myosdisk1",
+//         },
+//         imageReference: {
+//             publisher: "Canonical",
+//             offer: "0001-com-ubuntu-server-focal",
+//             sku: "20_04-lts-gen2",
+//             version: "latest",
+//         },
+//     },
+// });
